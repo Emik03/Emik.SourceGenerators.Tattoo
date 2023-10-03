f2c80e5ca1d7d6ce5d8bfdd521e921af07eef762
Emik <emik.lakunishok@gmail.com>
2023-10-03 19:17:15 +0200

    Add needed using for Morsels

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -19,6 +19,7 @@ global using System.Runtime.InteropServices;
 global using System.Runtime.Serialization;
 global using System.Security;
 global using System.Text;
+global using System.Text.RegularExpressions;
 global using System.Threading;
 global using Emik.Morsels;
 global using Gu.Roslyn.AnalyzerExtensions;
